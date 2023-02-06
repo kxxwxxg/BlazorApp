21b23c7b2f51d490e67a34bee2c5413956014d55
kxxwxxg <80820943+kxxwxxg@users.noreply.github.com>
2023-02-06 18:35:08 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 import time
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome('/home/jordi/chrome-selenium-python/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('http://www.google.com/');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
